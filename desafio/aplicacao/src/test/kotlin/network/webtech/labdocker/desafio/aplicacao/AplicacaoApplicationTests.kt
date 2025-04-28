0476c65ba37a1f1bcf649fa7e1760513f9c8b236
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2025-04-28 16:07:40 -0300

    Gerando aplicação no 'start.spring.io'

---
+++ desafio/aplicacao/src/test/kotlin/network/webtech/labdocker/desafio/aplicacao/AplicacaoApplicationTests.kt
@@ -0,0 +1,13 @@
+package network.webtech.labdocker.desafio.aplicacao
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AplicacaoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
